1c17c8009e19318343a0aa6c481bae161d054d4b
coa205 <coa205@naver.com>
2017-03-09 10:22:45 +0900

    Student DTO create

---
+++ src/main/java/kr/or/dgit/mybatis_study/dto/Student.java
@@ -0,0 +1,57 @@
+package kr.or.dgit.mybatis_study.dto;
+
+import java.util.Date;
+
+public class Student {
+	private int studentId;
+	private String name;
+	private String email;
+	private Date dob;
+	
+	public Student() {}
+
+	public Student(int studentId, String name, String email, Date dob) {
+		super();
+		this.studentId = studentId;
+		this.name = name;
+		this.email = email;
+		this.dob = dob;
+	}
+	
+	public int getStudentId() {
+		return studentId;
+	}
+	
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Date getDob() {
+		return dob;
+	}
+	
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s, %s, %s, %s", studentId, name, email, dob);
+	}
+}
